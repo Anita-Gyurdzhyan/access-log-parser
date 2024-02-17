ad5bb467b20b175f67ab73a833a636dd8bcdb598
Anita Gyurdzhyan <anita088@mail.ru>
2024-02-17 20:53:21 +0400

    Задание "Базовые типы данных Java"

--- src/main/java/homeWork2/Main.java
+++ src/main/java/homeWork2/Main.java
@@ -1,7 +1,27 @@
 package homeWork2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+       System.out.println("Введите первое число: ");
+       int number = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int number1 = new Scanner(System.in).nextInt();
+
+        int sum = number + number1;
+        int diff = number - number1;
+        int mult = number * number1;
+        double d = (double) number/number1;
+
+        System.out.println( "Сложение числе " + sum);
+        System.out.println("Разность чисел "+ diff);
+        System.out.println("Умножение чисел " + mult);
+        System.out.println("Деление чисел " + d);
+
+
+
+
     }
 }
